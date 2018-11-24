b2adc48116baa583d131cea75b3a2430588e423c
Alex <alexandracdunham@gmail.com>
2018-11-24 18:12:51 +0000

    deploy commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React, { Component } from 'react';
-import { Router } from "@reach/router";
 import Home from './containers/Home';
 import About from './containers/About';
 import './scss/main.scss'
 import Nav from './components/Nav';
 import Work from './containers/Work';
 import Footer from './components/Footer';
+import {
+    createMemorySource,
+    createHistory,
+    LocationProvider,
+    Router
+  } from "@reach/router"
+  
+  let source = createMemorySource("/starting/url")
+  let history = createHistory(source)
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <Nav/>
-                <main className="wrapper">
-                    <Router>
-                        <Home path="/"/>
-                        <About path="/about"/>
-                        <Work path="/work"/>
-                    </Router>
-                </main>
-                <Footer/>
-            </div>
+            <LocationProvider history={history}>
+                <div>
+                    <Nav/>
+                    <main className="wrapper">
+                        <Router>
+                            <Home path="/"/>
+                            <About path="/about"/>
+                            <Work path="/work"/>
+                        </Router>
+                    </main>
+                    <Footer/>
+                </div>
+            </LocationProvider>
         );
     }
 }
